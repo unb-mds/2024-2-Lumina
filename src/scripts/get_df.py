a80c0ee4a538f92143c112b0789377ac1e0791db
Caio Pacheco <caiopachecos29@gmail.com>
2025-02-17 14:51:01 -0300

    fix: sentiment graphic displays correctly

--- src/scripts/get_df.py
+++ src/scripts/get_df.py
@@ -2,19 +2,25 @@ from pandas import DataFrame
 from comentario.models import Comentario
 
 
-def get_proposal_df(proposal_number: int) -> DataFrame:
+def get_proposal_dic(proposal_number: int) -> dict:
     comment_list = Comentario.objects.filter(proposta_id=proposal_number)
-    print(comment_list)
-    print(len(comment_list))
-    nomes = ['Muito Negativa', 'Negativa', 'Neutra', 'Positiva', 'Muito Positiva']
-    result_df = [0, 0, 0, 0, 0]
 
-    # if len(comment_list) != 0:
-    for comment in comment_list:
-        if 0 <= comment.sentiment <= 4:
-            result_df[comment.sentiment] += 1
-    print(result_df)
-    df = DataFrame(data={'Opinião': nomes,
-                         'Comentários': result_df})
+    labels = ['Muito Negativa', 'Negativa', 'Neutra', 'Positiva', 'Muito Positiva']
+    values = [0, 0, 0, 0, 0]
+
+    if len(comment_list) != 0:
+        for comment in comment_list:
+            if 0 <= comment.sentiment <= 4:
+                values[comment.sentiment] += 1
+
+    dic = {'Opinião': labels,
+           'Comentários': values}
+    print(dic)
+    return dic
+
+
+def get_proposal_df(proposal_number: int) -> DataFrame:
+    data = get_proposal_dic(proposal_number)
+    df = DataFrame(data={'Opinião': data['Opinião'], 'Comentários': data['Comentários']})
     print(df)
     return df
